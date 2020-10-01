001f5ddcadd8bd4e142c1c3214ec588d8e07d21b
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-10-01 21:10:50 +0200

    Fix: Reworks initNode to return allocated pointer

--- tree.h
+++ tree.h
@@ -7,7 +7,7 @@
  * Nodes called root or parent are assumed to
  * be initialized with real board and values
  */
-void initNode(Node *node);
+Node *initNode();
 void createTree(Node *root, const size_t depth);
 Node *createChild(Node *parent, const size_t child);
 void createChildren(Node *parent);
