726c609f84e9d1b8b9149035cfe1296f754a49e5
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-10-01 18:16:49 +0200

    Adds missing definition for the shift function
    Why: This function was previously only used in board.c, so I left the
    declaration to be internal. Now it is used in other files (see tree.c),
    so I added it to board.h to be able to use it there.

--- board.h
+++ board.h
@@ -1,6 +1,7 @@
 #include "defs.h"
 #include <stdlib.h>
 
+uint64_t shift(const size_t row, const size_t col);
 void copyNode(Node *destination, const Node *origin);
 void copyBoard(Board *destination, const Board *origin);
 int makeMove(Node *node, const size_t column);
