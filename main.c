e80621b47cad0db3721a4f3bc1d857f6665f119d
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-10-02 17:13:06 +0200

    Changes default depth value from 6 to 4 in main

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    size_t depth = 6;
+    size_t depth = 4;
     Node *game = initNode();;
     if(argc == 2) {
         depth = atoi(argv[1]);
