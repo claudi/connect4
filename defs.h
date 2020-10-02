f45d9e3baf3daf61bc810262199851d0647b2049
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-10-02 20:38:25 +0200

    Adds ASSERT definition

--- defs.h
+++ defs.h
@@ -1,5 +1,6 @@
 #ifndef DEFS_GUARD
 #define DEFS_GUARD
+#include <stdlib.h>
 #include <stdint.h>
 
 #define N 8
@@ -19,4 +20,21 @@ struct Node {
     float heuristic;
 };
 
+#ifndef DEBUG
+    #define ASSERT(n) {}
+#else
+    #define ASSERT(n)                                   \
+    if(!(n)) {                                          \
+        fprintf(stderr, "\n\t  DEBUG INFO  \n");        \
+        fprintf(stderr, "\t Failed: %s\n", #n);         \
+        fprintf(stderr, "\t     On: %s\n", __DATE__);   \
+        fprintf(stderr, "\t     At: %s\n", __TIME__);   \
+        fprintf(stderr, "\tIn File: %s\n", __FILE__);   \
+        fprintf(stderr, "\tAt Line: %d\n", __LINE__);   \
+        fprintf(stderr, "\n");                          \
+        exit(1);                                        \
+    }
 #endif
+
+#endif
+
