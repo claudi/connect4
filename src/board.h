211d191c76ae765c665953c45c9c9015d31e7c67
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-11-01 17:33:01 +0100

    Changes name of input parameter in makeMove

--- src/board.h
+++ src/board.h
@@ -9,7 +9,7 @@ Mask shift(const size_t row, const size_t col);
 size_t pos2Shift(const size_t row, const size_t col);
 void copyNode(Node *destination, const Node *origin);
 void copyBoard(Board *destination, const Board *origin);
-int makeMove(Node *node, const size_t column);
+int makeMove(Node *node, const size_t col);
 void printBoard(const Node *node);
 void printNode(const Node *node);
 
