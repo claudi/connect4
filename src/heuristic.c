59cfae08a09b63eac3feb95891709e31663e6e20
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-11-01 23:03:30 +0100

    Adds max score to detected connect 4

--- src/heuristic.c
+++ src/heuristic.c
@@ -3,10 +3,13 @@
 #include "heuristic.h"
 
 int heuristic(const Node *node) {
-    int val1 = matches(node->board, match1);
-    int val2 = matches(node->board, match2);
-    int val3 = matches(node->board, match3);
     int val4 = matches(node->board, match4);
+    if(val4 > 0) {
+        return INT_MAX;
+    }
+    int val3 = matches(node->board, match3);
+    int val2 = matches(node->board, match2);
+    int val1 = matches(node->board, match1);
     return (val1 + 10*val2 + 100*val3 + 1000*val4);
 }
 
