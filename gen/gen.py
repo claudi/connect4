a54f6f684d97fa917f492f5d95e6f127e507ce22
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-11-28 09:43:35 +0100

    Imports different pseudocode for genetic algorithm

--- gen/gen.py
+++ gen/gen.py
@@ -1,73 +1,64 @@
-import random
+import numpy
+import pygad
+import pygad.nn
+import pygad.gann
 import subprocess
 
-POPULATION_SIZE = 100
-GENES = range(46080)
-GENES_SIZE = 1380
+def fitness_func(solution, sol_idx):
+    global GANN_instance, data_inputs, data_outputs
 
-class Individual(object):
-    chromosome = None
-    fitness = -1
+    predictions = pygad.nn.predict(last_layer=GANN_instance.population_networks[sol_idx],
+                                   data_inputs=data_inputs)
+    correct_predictions = numpy.where(predictions == data_outputs)[0].size
+    solution_fitness = (correct_predictions/data_outputs.size)*100
 
-    def __init__(self, chromosome):
-        self.chromosome = chromosome
-    
-    @classmethod
-    def mutated_gene(self):
-        '''
-        Create random genes for mutation
-        '''
-        global GENES
-        gene = random.choice(GENES)
-        return gene
-    
-    @classmethod
-    def create_genome(self):
-        '''
-        Create chromosome
-        '''
-        global GENES_SIZE
-        return [self.mutated_gene() for _ in range(GENES_SIZE)]
-    
-    def mate(self, par2):
-        '''
-        Perform matin and produce new offspring
-        '''
+    return solution_fitness
 
-        # chromosome for offspring
-        child_chromosome = []
-        for gp1, gp2 in zip(self.chromosome, par2.chromosome):
-            prob = random.random()
+def callback_generation(ga_instance):
+    global GANN_instance
 
-            if prob < 0.45:
-                child_chromosome.append(gp1)
-            
-            elif prob < 0.90:
-                child_chromosome.append(gp2)
+    population_matrices = pygad.gann.population_as_matrices(population_networks=GANN_instance.population_networks, 
+                                                            population_vectors=ga_instance.population)
 
-            else:
-                child_chromosome.append(self.mutated_gene())
+    GANN_instance.update_population_trained_weights(population_trained_weights=population_matrices)
 
-        return Individual(child_chromosome)
-    
-    def calculate_fitness(self, population):
-        self.fitness = 0
+    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+    print("Accuracy   = {fitness}".format(fitness=ga_instance.best_solution()[1]))
 
-        for rival in population:
-            self.fitness += 1
-            # self.fitness += self.match(rival)
-        
-        return self.fitness
-    
-    def match(self, rival):
-        score = 0
-        for chr1, chr2 in zip(self.chromosome, rival.chromosome):
-            if chr1 > chr2:
-                score += 1
-            elif chr1 < chr2:
-                score -= 1
+data_inputs = numpy.array([[1, 1],
+                           [1, 0],
+                           [0, 1],
+                           [0, 0]])
 
-        return score
+data_outputs = numpy.array([0, 
+                            1, 
+                            1, 
+                            0])
+
+GANN_instance = pygad.gann.GANN(num_solutions=5,
+                                num_neurons_input=2,
+                                num_neurons_hidden_layers=[2],
+                                num_neurons_output=2,
+                                hidden_activations=["relu"],
+                                output_activation="softmax")
+
+population_vectors = pygad.gann.population_as_vectors(population_networks=GANN_instance.population_networks)
+
+ga_instance = pygad.GA(num_generations=50, 
+                       num_parents_mating=3, 
+                       initial_population=population_vectors.copy(),
+                       fitness_func=fitness_func,
+                       mutation_percent_genes=5,
+                       callback_generation=callback_generation)
+
+ga_instance.run()
+
+ga_instance.plot_result()
+
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print(solution)
+print(solution_fitness)
+print(solution_idx)
 
 def runGames(population):
     POPULATION_SIZE = 10
