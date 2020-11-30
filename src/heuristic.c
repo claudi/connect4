bea4f7c1e7496caeb42a5e4500d8bdaae095308f
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-11-30 17:29:30 +0100

    Adds a basic heuristic

--- src/heuristic.c
+++ src/heuristic.c
@@ -2,6 +2,16 @@
 #define HEURISTIC_GUARD
 #include "heuristic.h"
 
+int myHeuristic(Board *board, const Side __attribute((unused)) side) {
+    int heuristic = 0;
+
+    heuristic += 2*(matches(board, match1) + 5*matches(board, match2) + 15*matches(board, match3));
+    board[TURN] ^= board[BOTH];
+    heuristic -= matches(board, match1) + 5*matches(board, match2) + 10*matches(board, match3);
+
+    return heuristic;
+}
+
 int controlHeuristic(Board *board, const Side __attribute((unused)) side) {
     int heuristic = 0;
 
@@ -34,7 +44,7 @@ int heuristic(const Node *node, const Side side) {
         return INT_MAX;
     }
 
-    return controlHeuristic(board, side);
+    return myHeuristic(board, side);
 }
 
 #endif // HEURISTIC_GUARD
