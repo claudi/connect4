28a639037c801c88aefa319d7722f7009c2364a9
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-01 20:21:08 +0100

    Start red-black branch

---
+++ src/red-black.c
@@ -0,0 +1,6 @@
+#include "red-black.h"
+
+void printRBTree(void) {
+    printf("Not done yet\n");
+}
+
---
+++ src/red-black.h
@@ -0,0 +1,6 @@
+#ifndef RED_BLACK_GUARD
+#define RED_BLACK_GUARD
+#include "defs.h"
+#include <stdio.h>
+
+#endif // RED_BLACK_GUARD
