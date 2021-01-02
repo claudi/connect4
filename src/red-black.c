c90976828e76ca03feac249d1146645d6e2af5a6
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-03 00:27:57 +0100

    Add first functions and definition for Keys

--- src/red-black.c
+++ src/red-black.c
@@ -1,6 +1,29 @@
 #include "red-black.h"
 
+Key keyShift(const short row, const short col) {
+    return (((Key) 1) << pos2Shift(row, col));
+}
+
+void printKey(const Key key) {
+    for(short iter_i = (2*N - 1); iter_i >= 0; iter_i--) {
+        for(short iter_j = 0; iter_j < N; iter_j++) {
+            printf("%c", (key & keyShift(iter_i, iter_j)) ? '1' : '0');
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+Key boardToKey(const Board *board) {
+    return ((* ((Key *) &board[TURN])) << N*N) + ((Key) board[BOTH]);
+    Key key = * ((Key *) &board[TURN]);
+    key = key << N*N;
+    key = key + ((Key) board[BOTH]);
+    return key;
+}
+
 void printRBTree(void) {
+    printf("%lu\n", sizeof(Key));
     printf("Not done yet\n");
 }
 
--- src/red-black.h
+++ src/red-black.h
@@ -1,6 +1,13 @@
 #ifndef RED_BLACK_GUARD
 #define RED_BLACK_GUARD
 #include "defs.h"
+#include "board.h"
 #include <stdio.h>
+
+typedef __uint128_t Key;
+
+void printKey(const Key key);
+Key boardToKey(const Board *board);
+void printRBTree(void);
 
 #endif // RED_BLACK_GUARD
