2218be6b3a87c7b08c2e1bef1ca758550b0d20b6
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-15 10:57:31 +0100

    Change sorting function to the heuristic function

--- src/tree.c
+++ src/tree.c
@@ -50,7 +50,7 @@ void orderChildren(Node *parent) {
         if(wonBoard(parent->child[iter]->board)) {
             values[iter] = INT_MAX;
         } else {
-            values[iter] = matches(parent->child[iter]->board, match3);
+            values[iter] = heuristic(parent->child[iter], next(parent->turn));
         }
     }
 
