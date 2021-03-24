afe733edb92db06a2c2d464c47e105f24bb6ac9e
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-03-24 12:27:14 +0100

    Add escape sequences definitions (for Linux only)
    Following: https://www.linuxhowtos.org/Tips%20and%20Tricks/ansi_escape_sequences.htm

--- src/bling.h
+++ src/bling.h
@@ -12,6 +12,11 @@
 #define SIDE_H 9
 #define CREDITS_W 28
 
+#ifdef __linux__
+#define colorON  "\033[0;7m" // inverted background
+#define colorOFF "\033[0;0m" // default
+#endif // __linux__
+
 void printInterface(const Game *game);
 void humanInput(Game *game);
 Game *initGame(void);
