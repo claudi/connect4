b1371cfe59ff431ab77eac8672004f8b5dbfb1d7
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-08-04 18:36:09 +0200

    Add headless functionality

--- src/headless.c
+++ src/headless.c
@@ -1,6 +1,13 @@
 #include "headless.h"
 
 int headless_main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv) {
+    Game *game = initGame();
+    machineMove(game);
+
+    printf("%u\n", game->stats.exploredPositions);
+
+    free(game->node);
+    free(game);
     return EXIT_SUCCESS;
 }
 
--- src/headless.h
+++ src/headless.h
@@ -1,6 +1,8 @@
 #ifndef HEADLESS_GUARD
 #define HEADLESS_GUARD
 #include "defs.h"
+#include "game.h"
+#include "minimax.h"
 
 int headless_main(int argc, char **argv);
 
