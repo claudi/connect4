2fb38e1c70161bb5194a8a46138506b3e913d5bd
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-08-15 22:43:38 +0200

    Store commands array size in a constant variable

--- src/commands.c
+++ src/commands.c
@@ -7,13 +7,14 @@ command commands[] = {
 };
 
 int handle_commands(int argc, char **argv) {
+    const size_t ncommands = ARRAY_SIZE(commands);
     if(argc < 2) {
         return EXIT_FAILURE;
     }
 
     const char *cmd = argv[1];
 
-    for(size_t iter = 0; iter < ARRAY_SIZE(commands); iter++) {
+    for(size_t iter = 0; iter < ncommands; iter++) {
         if(strcmp(cmd, commands[iter].name) == 0) {
             return commands[iter].function(argc, argv);
         }
