3a9a80a3ded21c4dfef46174541db7578389c1c9
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-09-03 17:50:20 +0200

    Add start of neural network

---
+++ src/neural-network.h
@@ -0,0 +1,20 @@
+#ifndef NEURALNETWORK_GUARD
+#define NEURALNETWORK_GUARD
+#include "defs.h"
+
+typedef int Num;
+typedef size_t *Topology;
+
+typedef struct {
+    size_t ncells;
+    Num **weights;
+} Layer;
+
+typedef struct {
+    size_t ninputs;
+    size_t nlayers;
+    Layer *layers;
+} Network;
+
+#endif // NEURALNETWORK_GUARD
+
