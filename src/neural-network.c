06bc82369d797a65668ccfd12101a31930925c7a
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-09-07 18:48:09 +0200

    Add forward propagation (to be tested)

--- src/neural-network.c
+++ src/neural-network.c
@@ -28,3 +28,74 @@ Network initNetwork(const size_t nlayers, const Topology topology) {
     };
 }
 
+void printNetwork(const Network *network) {
+    printf("Topology with %zu layers\n", network->nlayers);
+    printf("\t%zu ", network->ninputs);
+    for(size_t iter = 0; iter < network->nlayers; iter++) {
+        printf("%2zu ", network->layers[iter].ncells);
+    }
+    printf("\n");
+    printf("Layer 0 with %zu cells\n", network->layers[0].ncells);
+    for(size_t cell = 0; cell < network->layers[0].ncells; cell++) {
+        printf("\t");
+        for(size_t weight = 0; weight < network->ninputs; weight++) {
+            printf("%2d ", network->layers[0].weights[cell][weight]);
+        }
+        printf("\n");
+    }
+    for(size_t iter = 1; iter < network->nlayers; iter++) {
+        printf("Layer %zu with %zu cells\n", iter, network->layers[iter].ncells);
+        for(size_t cell = 0; cell < network->layers[iter].ncells; cell++) {
+            printf("\t");
+            for(size_t weight = 0; weight < network->layers[iter-1].ncells; weight++) {
+                printf("%d ", network->layers[iter].weights[cell][weight]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+Num *evaluateNetwork(const Network *network, const Num *input) {
+    ASSERT(network != NULL);
+    ASSERT(input != NULL);
+
+    size_t maxWidth = 0;
+    if(maxWidth < network->ninputs) {
+        maxWidth = network->ninputs;
+    }
+    for(size_t iter = 0; iter < network->nlayers; iter++) {
+        size_t ncells = network->layers[iter].ncells;
+        if(maxWidth < ncells) {
+            maxWidth = ncells;
+        }
+    }
+    ASSERT(maxWidth > 0);
+
+    Num *prev = (Num *) malloc(maxWidth * sizeof(Num));
+    for(size_t iter = 0; iter < network->ninputs; iter++) {
+        prev[iter] = input[iter];
+    }
+
+    Num *output = (Num *) malloc(maxWidth * sizeof(Num));
+    for(size_t iter = 0; iter < network->ninputs; iter++) {
+        output[iter] = 0;
+    }
+
+    for(size_t cell = 0; cell < network->layers[0].ncells; cell++) {
+        for(size_t weight = 0; weight < network->ninputs; weight++) {
+            output[cell] += prev[weight] * network->layers[0].weights[cell][weight];
+        }
+    }
+
+    for(size_t iter = 1; iter < network->nlayers; iter++) {
+        for(size_t cell = 0; cell < network->layers[iter].ncells; cell++) {
+            output[cell] = 0;
+            for(size_t weight = 0; weight < network->layers[iter-1].ncells; weight++) {
+                output[cell] += prev[weight] * network->layers[iter].weights[cell][weight];
+            }
+        }
+    }
+
+    return output;
+}
+
