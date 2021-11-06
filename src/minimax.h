cdb40c99f85265c95703a097b81a4b7326b07302
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-11-06 16:49:27 +0100

    Clean minimax.h

--- src/minimax.h
+++ src/minimax.h
@@ -1,23 +1,6 @@
 #ifndef MINIMAX_GUARD
 #define MINIMAX_GUARD
-#include "defs.h"
-#include "tree.h"
-#include "table.h"
 #include "game.h"
-#include "heuristic.h"
-#include "tree.h"
-#include <time.h>
-
-typedef struct {
-    long alpha;
-    long beta;
-} Prune;
-
-typedef struct {
-    short depth;
-    const Side side;
-    Bool maximizing;
-} MinimaxStatus;
 
 void machineMove(Game *game);
 
