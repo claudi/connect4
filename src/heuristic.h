cc411955b8c92ef3e98767cfe680d33fac8b1a11
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-11-06 16:47:32 +0100

    Clean heuristic.h

--- src/heuristic.h
+++ src/heuristic.h
@@ -1,9 +1,7 @@
 #ifndef HEURISTIC_GUARD
 #define HEURISTIC_GUARD
-#include "board.h"
 #include "defs.h"
 #include "masks.h"
-#include <stdlib.h>
 
 long getHeuristic(const Node *node, const Side side);
 long getOrderingHeuristic(const Node *node, const Side side);
